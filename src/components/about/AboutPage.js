9b91c7180df0e19d6ae76fda1b4aa86a2e8e1363
robjar <robjar@wp.eu>
2018-05-17 23:18:14 +0200

    init app structure

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class AboutPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>About</h1>
+        <p>This application is awesome!</p>
+      </div>
+    );
+  }
+}
+
+export default AboutPage;
+
