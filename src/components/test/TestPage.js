d2d45de9d8cee88961e494cc4ad6b412cea15c1c
robjar <robjar@wp.eu>
2018-05-18 19:52:19 +0200

    add TestPage

---
+++ src/components/test/TestPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class TestPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>TestPage</h1>
+        <p>...</p>
+      </div>
+    );
+  }
+}
+
+export default TestPage;
